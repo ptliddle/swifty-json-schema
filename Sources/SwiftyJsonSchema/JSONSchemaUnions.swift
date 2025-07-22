cfc92524002837d32614eac51f4cc44e07e75b65
Peter Liddle <development.peter@liddle.network>
2025-07-22 12:53:57 -0700

    Fix `any` issue causing compilation failure on Swift 6+

--- Sources/SwiftyJsonSchema/JSONSchemaUnions.swift
+++ Sources/SwiftyJsonSchema/JSONSchemaUnions.swift
@@ -7,9 +7,9 @@
 
 /// This is used when you have a type that wraps other concrete types that in JSON would be a union. I.e. a role key that can have Admin, User, etc role objects
 public protocol JSONSchemaAnyOfDiscriminatedUnion: JSONSchemaGeneratable {
-    var allowedTypes: [ProducesJSONSchema.Type] { get }
+    var allowedTypes: [any ProducesJSONSchema.Type] { get }
 }
 
 public protocol JSONSchemaOneOfDiscriminatedUnion: JSONSchemaGeneratable {
-    var allowedTypes: [ProducesJSONSchema.Type] { get }
+    var allowedTypes: [any ProducesJSONSchema.Type] { get }
 }
